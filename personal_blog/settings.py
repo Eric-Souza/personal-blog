cd4e77eec3bae303830aa0961f238775dc0cb3c7
Eric Souza <eric.bh18souza@gmail.com>
2021-02-05 12:57:15 -0300

    dir change

--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -120,5 +120,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/./src/static/css/blog.css/'
 STATIC_ROOT = os.path.join(BASE_DIR, "../static/css/blog.css")
